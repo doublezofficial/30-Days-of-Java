f48db207d91a653364b1747aba8c989b8cb572d6
doublezofficial <mzuhaib2@gmail.com>
2020-10-30 00:21:33 +0530

    Builder pattern for class hierarchies

--- src/com/doublezofficial/Main.java
+++ src/com/doublezofficial/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         NutritionFacts cocaCola = new NutritionFacts.Builder(210,8).fat(0).calories(27).carbohydrates(29).build();
+        NyPizza nyPizza = new NyPizza.Builder(NyPizza.Size.MEDIUM).addTopping(Pizza.Topping.HAM).build();
+
         System.out.println(cocaCola);
     }
 }
